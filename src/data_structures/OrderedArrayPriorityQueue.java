110cdf79e2c70527655dcfbfb3888558214aaa81
Sky <erniesanchez619@gmial.com>
2020-02-11 13:57:55 -0800

    added comparaTo and iterartor

--- src/data_structures/OrderedArrayPriorityQueue.java
+++ src/data_structures/OrderedArrayPriorityQueue.java
@@ -1,3 +1,12 @@
+/**
+ *  Program # 1
+ *  Implement priority queue for an ordered array using FIFO.
+ *  Prioritize by value and time of arrival.
+ *  CS310
+ *  Date: 2/20/2020
+ *  @Autho: Ernesto Sanchez cssc1268
+ */
+
 package data_structures;
 
 public class OrderedArrayPriorityQueue {
--- src/data_structures/UnorderedArrayPriorityQueue.java
+++ src/data_structures/UnorderedArrayPriorityQueue.java
@@ -1,3 +1,12 @@
+/**
+ *  Program # 1
+ *  Implement priority queue for an unordered array using FIFO.
+ *  Prioritize by value and time of arrival.
+ *  CS310
+ *  Date: 2/20/2020
+ *  @Autho: Ernesto Sanchez cssc1268
+ */
+
 package data_structures;
 
 public class UnorderedArrayPriorityQueue {
