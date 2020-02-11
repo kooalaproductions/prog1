c048ed76424977e632e30ffe5fab30de4fe3ccb1
Sky <erniesanchez619@gmial.com>
2020-02-11 14:51:30 -0800

    finished peek method

--- src/data_structures/UnorderedArrayPriorityQueue.java
+++ src/data_structures/UnorderedArrayPriorityQueue.java
@@ -14,6 +14,7 @@ import java.util.Iterator;
 public class UnorderedArrayPriorityQueue <E extends Comparable<E>> implements PriorityQueue<E> {
     private E[] array;
     private int maxSize;
+    private int currentSize;
 
 
     public UnorderedArrayPriorityQueue(){//first constructor
@@ -22,17 +23,40 @@ public class UnorderedArrayPriorityQueue <E extends Comparable<E>> implements Pr
 
     public UnorderedArrayPriorityQueue(int size){//second constructor
         maxSize = size;
+        currentSize =0;
+        array = (E[]) new Object[maxSize];
     }
 
 
     @Override
     public boolean insert(E object) {
-        return false;
+        if(isFull()){//will check if full
+            return false;
+        }
+        array[currentSize++] = object;//if not full return true
+        return true;
     }
 
     @Override
     public E remove() {
-        return null;
+        if(isEmpty()){//returns null if empty
+            return null;
+        }
+        int highPriority = 0;
+        E highElement = array[0];
+
+        for(int i = 1; i < currentSize; i++){
+            if(((Comparable<E>)array[i]).compareTo(highElement) > 0){
+                highPriority = i;
+                highElement = array[i];
+            }
+        }
+        for(int i = highPriority; i < currentSize - 1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+        currentSize--;
+        return highElement;//returns object of high priority and has been in the queue the longest
     }
 
     @Override
